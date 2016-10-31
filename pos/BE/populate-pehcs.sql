1bdf5102fa5c08879b683ad1c32310caa31d39a9
Tano Fotang <tano.fotang@pethersolutions.com>
2016-10-31 09:29:01 +0100

    fixed data too long for primary key columns

--- pos/BE/populate-pehcs.sql
+++ pos/BE/populate-pehcs.sql
@@ -1,3 +1,4 @@
+delete from exception_types;
 insert into exception_types(ex_code, name, descr) values
 ('nomatch', 'ex_match_failed', 'Biometric verification failed'),
 ('scstale','ex_card_is_stale','Card needs to be updated'),
@@ -7,71 +8,83 @@ insert into exception_types(ex_code, name, descr) values
 ('noresume', 'ex_cant_resume', 'Cannot resume visit (e.g. because verification failed)'),
 ('exvisit', 'ex_visit_timdeout', 'Visit timed out');
 
+delete from verification_types;
 insert into verification_types(veri_type_id, name, descr) values
-('fprint', 'veri_fingerprint_verif', 'Fingerprint verification'),
+('fp', 'veri_fprint', 'Fingerprint verification'),
 ('face', 'veri_facial_recog', 'Facial recognition'),
 ('iris', 'veri_iris_auth', 'Iris-pattern authentication');
 
+delete from visit_types;
 insert into visit_types(visit_type_id, name, descr) values
 ('new', 'vis_fresh_visit', 'Fresh, new visit'),
 ('folup', 'vis_follow_up_visit', 'Follup up visit'),
 ('other','vis_other_visit', 'Other type of visit');
 
+delete from dispensation_state_types;
 insert into dispensation_state_types(disp_state_id, name, descr) values
 ('init','disp_started', 'Started'),
 ('done', 'disp_completed', 'Completed'),
 ('ok','disp_ok', 'Approval not required'),
-('pending','disp_approval_pending', 'Approval pending')
+('pendi','disp_apprvl_pending', 'Approval pending')
 /*,('approved','disp_approved', 'Approved')
 (',rejected','disp_rejected', 'Not approved--rejected')*/;
 
+delete from approval_resp_groups;
 insert into approval_resp_groups(resp_group_id, name, descr) values
-('approved','app_res_approved','Approved'),
-('rejected','app_res_rejected','Rejected'),
-('tryagain','app_res_delay', 'Rejected now, but try later');
+('app','app_res_approved','Approved'),
+('rej','app_res_rejected','Rejected'),
+('retry','app_res_delay', 'Rejected now, but try later');
 
+delete from transmission_modes;
 insert into transmission_modes(tx_mode_id,  name, descr) values
 ('sms','tx_sms', 'SMS'),
 ('email','tx_email', 'E-mail')
 /*,('other','tx_other' ,'Other')*/
 ;
 
+delete from datatypes;
 insert into datatypes(datatype, name, descr) values
 ('text','datatype_text','text'),
 ('int','datatype_int','whole number'),
 ('float','datatype_float','decimal number');
 
+delete from productcat_detail_types;
 insert into productcat_detail_types(name, datatype, descr) values
-('uprice','prod_unit_price','unit price'),
-('qty', 'prod_unit_qty', 'quantity');
+('prod_unit_price', 'float','unit price'),
+('prod_unit_qty', 'float', 'quantity');
 
-insert into product_categories(catId, name, descr) values
-('drug', 'cat_drus', 'Drugs'),
-('consult','cat_consultation', 'Consultation'),
-('mat','cat_material', 'Material'),
-('lab','cat_lab_test', 'Laboratory test'),
-('deliv','cat_delivery', 'Child delivery'),
-('vac','cat_vaccine', 'Vaccination'),
-('os','cat_others', 'Other Services');
+delete from product_categories;
+insert into product_categories(catId, name, descr, sorter) values
+(11000,'cat_drugs', 'Drugs', 1),
+(12000,'cat_material', 'Material', 25),
+(13000,'cat_vaccine', 'Vaccination',40),
+(21000,'cat_consultation', 'Consultation', 5),
+(22000,'cat_lab_test', 'Laboratory test', 10),
+(24000,'cat_hospital', 'Hospitalisation',15),
+(25000,'cat_delivery', 'Child delivery',20),
+(90000,'cat_others', 'Other Services',999);
 
+delete from units;
 insert into units(unitId, name, descr) values
 ('none','unit_none','NA'),
 ('tabs', 'unit_tablet', 'tablet'),
-('packet','unit_packet','packet'),
+('pack','unit_packet','packet'),
 ('l','unit_litre','litre'),
 ('kg','unit_kg','kg'),
 ('day', 'unit_day','day'),
 ('other','unit_other','other');
 
 -- seek approval if...
+delete from approval_reqmt_types;
 insert into approval_reqmt_types(reqtypeId, name, descr) values
 ('cost', 'apreq_cost', 'total cost exceeds threshold'),
 ('qty', 'apreq_quantity', 'quantity exceeds threshold');
 -- approval is independent of the caps
 
+delete from insurer_status_types;
 insert into insurer_status_types(typeId, name, descr) values
-('active','ins_status_active', 'Active'),
+('act','ins_status_active', 'Active'),
 ('inact', 'ins_status_inactive', 'Disabled'),
-('expired','ins_status_expired','Expired');
+('expir','ins_status_expired','Expired');
 
 
