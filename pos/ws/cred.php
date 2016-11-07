fb6b8e07eda2c2dc744a64e8ec0c2ccfc63f847b
Tano Fotang <tano.fotang@pethersolutions.com>
2016-11-08 00:19:45 +0100

    new file, sample db credentials

---
+++ pos/ws/cred.php
@@ -0,0 +1,12 @@
+
+<?php
+/*
+fotang, Fri Nov  4 18:54:38 WAT 2016
+DB login details
+*/
+
+	$cred_host 		=   "p:localhost";
+	$cred_user   	=   "mike";
+	$cred_pass     	=   "mtf";
+	$cred_db       	=   "pehcs" 
+?>
