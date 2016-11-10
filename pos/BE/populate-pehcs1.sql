be9ab14e8a82d9b540e2c5f21d972916ad0e7e8d
Tano Fotang <tano.fotang@pethersolutions.com>
2016-11-10 07:49:29 +0100

    new permission types for dispensation

--- pos/BE/populate-pehcs1.sql
+++ pos/BE/populate-pehcs1.sql
@@ -17,3 +17,22 @@ INSERT INTO PermissionTypes(mode,Description, sort) VALUES
 	('ADDORGCONTACT','add organisation contact person', 1041),
 	('UPDORGCONTACT','update contact person details', 1042),
 	('DELORGCONTACT','remove contact person', 1043);
+INSERT INTO PermissionTypes(mode,Description, sort) VALUES
+	('DEAORG','Deactivate service provider',1050),
+	('ACTORG','Activate service provider', 1055),
+	('ADDINSURER','Add insurer',2000),
+	('UPDINSURER','Modify insurer',2001),
+	('DELINSURER','Remove insurer',2002),
+/*	('ADDBENIF','Add beneficiary', 2200), -- */
+	('DELBENIF','Remove beneficiary', 2202),
+	('UPDBENIF','Modify beneficiary', 2203),
+	('ADDPACKG','Add a package', 2300),
+	('UPDPACKG','Modify a package', 2302),
+	('DELPACKG','Remove a package', 2303),
+	('ADDVISIT','Add a visitation',2400),
+	('ENDVISIT','Close a visitation',2401),
+	('RESVISIT','Resume a visitation',2402),
+	('ADDDISP','Make a dispensation',2410),
+	('UPDDISP','Modify a dispensation',2412),
+	('DELDISP','Remove a dispensation',2413),
+	('RDDISP','See dispensations',2415);
